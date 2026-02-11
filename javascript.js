7384c4dffc0c894f77dff47fe70dea58c8ea63f9
haulo <233421461+haulohaulo@users.noreply.github.com>
2026-02-11 18:40:30 +0000

    Display Default Values If Form Inputs Are Empty

--- javascript.js
+++ javascript.js
@@ -7,12 +7,12 @@ const player2Display = document.getElementById("p2");
 const numberOfRounds = document.querySelector(".roundDisplay");
 
 let player1 = {
-    name: "Player1",
+    name: "Player 1",
     mark: ["cross", "./marks/cross-svgrepo-com.svg"]
 }
 
 let player2 = {
-    name: "Player2",
+    name: "Player 2",
     mark: ["circle", "./marks/circle-svgrepo-com.svg"]
     
 }
@@ -47,9 +47,19 @@ function playerInput() {
     form.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        player1.name = document.getElementById("player1-Name").value;
-        player2.name = document.getElementById("player2-Name").value;
-        gameflow.round = document.getElementById("rounds").value;
+        const player1NameValue = document.getElementById("player1-Name").value;
+        const player2NameValue = document.getElementById("player2-Name").value;
+        const roundValue = document.getElementById("rounds").value;
+        if (player1NameValue) {
+            player1.name = player1NameValue;
+        }
+        if (player2NameValue) {
+            player2.name = player2NameValue;
+        }
+        if (roundValue) {
+            gameflow.round = roundValue;
+        }
+        
 
         player1Display.textContent = player1.name;
         player2Display.textContent = player2.name;
